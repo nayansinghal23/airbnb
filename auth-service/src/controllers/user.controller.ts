006de20255d0126eb6bcdaa154253c2ce5805baa
Nayan Singhal <nayansinghal393@gmail.com>
2026-08-26 19:50:59 +0530

    Handling errors gracefully

--- auth-service/src/controllers/user.controller.ts
+++ auth-service/src/controllers/user.controller.ts
@@ -2,65 +2,78 @@ import { Request, Response } from 'express';
 
 import { encryptPassword } from '../utils/encryptPassword';
 import { generateJWTToken } from '../utils/generateJWTToken';
-import { verifyJWTToken } from '../utils/verifyJWTToken';
 import { verifyPassword } from '../utils/verifyPassword';
 
 import { createUserService, getUserProfile, loginService } from '../services/user.service';
 
 export const createUserController = async (req: Request, res: Response) => {
-    const hashedPassword = await encryptPassword(req.body.password);
-    if(!hashedPassword) {
+    try {
+        const hashedPassword = await encryptPassword(req.body.password);
+        if(!hashedPassword) {
+            return res.status(500).json({
+                message: "Unable to generate token"
+            });
+        }
+    
+        const user = await createUserService({
+            ...req.body,
+            password: hashedPassword,
+        });
+    
+        if(!user || !user.id) {
+            return res.status(500).json({
+                message: "Unable to create a user"
+            });
+        }
+    
+        const token = generateJWTToken(user.id, user.email);
+        res.cookie("access_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 60 * 60 * 1000,
+        }).status(201).json({
+            userId: user.id,
+        });
+    } catch (error) {
         return res.status(500).json({
-            message: "Unable to generate token"
+            success: false,
+            message: "Failed to create a user",
         });
     }
-
-    const user = await createUserService({
-        ...req.body,
-        password: hashedPassword,
-    });
-
-    if(!user || !user.id) {
-        return res.status(500).json({
-            message: "Unable to create a user"
-        });
-    }
-
-    const token = generateJWTToken(user.id, user.email);
-    res.cookie("access_token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 60 * 60 * 1000,
-    }).status(201).json({
-        userId: user.id,
-    });
 }
 
 export const loginController = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
-    const user = await loginService(email);
-
-    if(!user) {
-        return res.status(401).json({
-            message: "User not found"
-        });
-    }
-
-    const isPasswordMatch = await verifyPassword(password, user.password);
-    if(!isPasswordMatch) {
-        return res.status(401).json({
-            message: "Incorrect password"
-        });
-    }
+    try {
+        const { email, password } = req.body;
+        const user = await loginService(email);
     
-    const token = generateJWTToken(user.id, user.email);
-    return res.cookie("access_token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 60 * 60 * 1000,
-    }).status(200).json({
-        message: "Login successfull"
-    });
+        if(!user) {
+            return res.status(401).json({
+                message: "User not found"
+            });
+        }
+    
+        const isPasswordMatch = await verifyPassword(password, user.password);
+        if(!isPasswordMatch) {
+            return res.status(401).json({
+                message: "Incorrect password"
+            });
+        }
+        
+        const token = generateJWTToken(user.id, user.email);
+        return res.cookie("access_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 60 * 60 * 1000,
+        }).status(200).json({
+            message: "Login successfull"
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Failed to login",
+        });
+    }
 }
 
 export const getUserProfileController = async (req: Request, res: Response) => {
--- booking-service/src/controllers/booking.controller.ts
+++ booking-service/src/controllers/booking.controller.ts
@@ -3,26 +3,46 @@ import { Request, Response } from 'express';
 import { confirmBookingService, createBookingService } from '../services/booking.service';
 
 export const createBookingController = async (req: Request, res: Response) => {
-    const booking = await createBookingService(req.body);
-
-    if(!booking) {
-        return res.status(400).json({
+    try {
+        const booking = await createBookingService(req.body);
+    
+        if(!booking) {
+            return res.status(400).json({
+                success: false,
+                message: "Unable to create a booking"
+            })
+        }
+    
+        res.status(201).json({
+            bookingId: booking.bookingId,
+            idempotencyKey: booking.idempotencyKey,
+        });
+    } catch (error) {
+        return res.status(500).json({
             success: false,
-            message: "Unable to create a booking"
-        })
+            message: "Failed to create booking",
+        });
     }
-
-    res.status(201).json({
-        bookingId: booking.bookingId,
-        idempotencyKey: booking.idempotencyKey,
-    });
 }
 
 export const confrimBookingController = async (req: Request, res: Response) => {
-    const booking = await confirmBookingService(req.params.idempotencyKey);
+    try {
+        const booking = await confirmBookingService(req.params.idempotencyKey);
+        if(!booking) {
+            return res.status(400).json({
+                success: false,
+                message: "Unable to confirm a booking"
+            })
+        }
 
-    res.status(200).json({
-        bookingId: booking.id,
-        status: booking.status,
-    });
+        res.status(200).json({
+            bookingId: booking.id,
+            status: booking.status,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Failed to confirm booking",
+        });
+    }
 }
